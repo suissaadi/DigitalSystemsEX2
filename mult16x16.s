2c6375ba7f01e3012ad40816eca2c93e9f0af809
darnissan <67154616+darnissan@users.noreply.github.com>
2023-01-11 18:14:44 +0200

    fix for mult16x16

--- mult16x16.s
+++ mult16x16.s
@@ -1,7 +1,7 @@
 # Operands to multiply
 .data
-a: .word 0xBAD
-b: .word 0xFEED
+a: .word 0xbad
+b: .word 0xfeed
 
 .text
 main:   # Load data from memory
@@ -22,10 +22,33 @@ main:   # Load data from memory
         
 ####################
 # Start of your code
+# set a counter to 0
+slli s1 t4 24
+srli s1 s1 24
+srli t4 t4 8
+add t1 t1 x0
+addi t1 t1 0
+addi t2 t2 2
+addi t1 t1 1
+mul t5 t3 s1
+and t5 t5 t0
+add t6 t6 t5
+add s1 t4 x0
+mul t5 t3 s1
+and t5 t5 t0
+slli t5 t5 8
+add t6 t6 t5
+
+
+# End of your code
+
 
 # Use the code below for 16x8 multiplication
 #   mul		<PROD>, <FACTOR1>, <FACTOR2>
 #   and		<PROD>, <PROD>, t0
+
+
+
 
 # End of your code
 ####################
@@ -35,5 +58,3 @@ finish: addi    a0, x0, 1
         ecall # print integer ecall
         addi    a0, x0, 10
         ecall # terminate ecall
-
-

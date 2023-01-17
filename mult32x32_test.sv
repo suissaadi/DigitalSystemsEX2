dbe1f6235013687bf9509d69873b5b3dd9ea81b0
suissaadi <adi.suissa@campus.technion.ac.il>
2023-01-17 20:02:12 +0200

    testbench for regular 32x32 multiplier

--- mult32x32_test.sv
+++ mult32x32_test.sv
@@ -10,8 +10,44 @@ module mult32x32_test;
     logic [63:0] product; // Miltiplication product
 
 // Put your code here
-// ------------------
+
+	mult32x32 uut  (.clk(clk), 
+					.reset(reset), 
+					.start(start), 
+					.a(a), 
+					.b(b), 
+					.busy(busy), 
+					.product(product)
+	);
+	
+	initial begin
+	
+		clk = 1'b0;
+		start = 1'b0;
+		reset = 1'b1;
+		repeat (4) begin
+			@(posedge clk);
+		end
+		
+		reset = 1'b0;
+		a = 208459867;
+		b = 315157909;
+		@(posedge clk);
+		
+		start = 1'b1;
+		@(posedge clk);
+		@(negedge busy);
+	
+	end
+	
+	always begin
+		#10 clk = ~clk;
+	end
 
 // End of your code
 
 endmodule
+
+
+// id1 = 208459867
+// id2 = 315157909
